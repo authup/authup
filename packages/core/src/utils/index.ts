557751708136dcd9cb6a74719bf119c6eaac89d9
Peter Placzek <peter.placzek1996@gmail.com>
2021-07-11 12:19:37 +0200

    added npm publish workflow

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,5 +1,3 @@
-import {URL} from "url";
-
 export function hasOwnProperty<X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> {
     return obj.hasOwnProperty(prop);
 }
