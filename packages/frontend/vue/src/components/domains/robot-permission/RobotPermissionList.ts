5270d8a5b1229504d99683e3433c434a2fbb3e0f
tada5hi <peter.placzek1996@gmail.com>
2022-02-22 10:44:04 +0100

    created robot-permisison & robot-role lists + actions

---
+++ packages/frontend/vue/src/components/domains/robot-permission/RobotPermissionList.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import Vue, { CreateElement, VNode } from 'vue';
+import { Permission } from '@typescript-auth/domains';
+import {
+    RobotPermissionListItemActions,
+    RobotPermissionListItemActionsProperties,
+} from './RobotPermissionListItemActions';
+import { PermissionList } from '../permission';
+import { SlotName } from '../../constants';
+
+export type Properties = {
+    entityId: string
+};
+
+export const RobotPermissionList = Vue.extend<any, any, any, Properties>({
+    components: {
+        PermissionList,
+        RobotPermissionListItemActions,
+    },
+    props: {
+        entityId: {
+            type: String,
+            required: true,
+        },
+    },
+    render(createElement: CreateElement): VNode {
+        const vm = this;
+        const h = createElement;
+
+        const buildProps = (item: Permission) : RobotPermissionListItemActionsProperties => ({
+            robotId: vm.entityId,
+            permissionId: item.id,
+        });
+
+        return h(PermissionList, {
+            scopedSlots: {
+                [SlotName.ITEM_ACTIONS]: (slotProps) => h(RobotPermissionListItemActions, {
+                    props: buildProps(slotProps.item),
+                }),
+            },
+        });
+    },
+});
