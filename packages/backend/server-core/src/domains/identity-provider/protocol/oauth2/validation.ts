61d4e9dbad377e2555376a5b662868113247801f
tada5hi <peter.placzek1996@gmail.com>
2022-09-05 17:02:39 +0200

    removed validator dependency

--- packages/backend/server-core/src/domains/identity-provider/protocol/oauth2/validation.ts
+++ packages/backend/server-core/src/domains/identity-provider/protocol/oauth2/validation.ts
@@ -5,73 +5,26 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-import { OAuth2IdentityProvider, OAuth2IdentityProviderBase, hasOwnProperty } from '@authelion/common';
-import validator from 'validator';
-import { ValidatorError } from '../../../../config';
+import { OAuth2IdentityProviderBase } from '@authelion/common';
+import zod from 'zod';
+import { BadRequestError } from '@typescript-error/http';
+
+const validationSchema = zod.object({
+    token_url: zod.string().url(),
+    token_revoke_url: zod.string().url().optional(),
+    authorize_url: zod.string().url(),
+    user_info_url: zod.string().url().optional(),
+    scope: zod.string().min(3).max(2000).optional(),
+    client_id: zod.string().min(3).max(128),
+    client_secret: zod.string().min(3).max(128).optional(),
+});
 
 export function validateOAuth2IdentityProviderProtocol<
     T extends Partial<OAuth2IdentityProviderBase>,
 >(entity: T) : T {
-    if (
-        typeof entity.token_url !== 'string' ||
-        !validator.isURL(entity.token_url)
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'token_url' });
-    }
-
-    if (
-        hasOwnProperty(entity, 'token_revoke_url') &&
-        (
-            typeof entity.token_revoke_url !== 'string' ||
-            !validator.isURL(entity.token_revoke_url)
-        )
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'token_revoke_url' });
-    }
-
-    if (
-        typeof entity.authorize_url !== 'string' ||
-        !validator.isURL(entity.authorize_url)
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'authorize_url' });
-    }
-
-    if (
-        hasOwnProperty(entity, 'user_info_url') &&
-        (
-            typeof entity.user_info_url !== 'string' ||
-            !validator.isURL(entity.user_info_url)
-        )
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'user_info_url' });
-    }
-
-    if (
-        hasOwnProperty(entity, 'scope') &&
-        (
-            typeof entity.scope !== 'string' ||
-            !validator.isLength(entity.scope, { min: 3, max: 2000 })
-        )
-
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'scope' });
-    }
-
-    if (
-        typeof entity.client_id !== 'string' ||
-        !validator.isLength(entity.client_id, { min: 3, max: 128 })
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'client_id' });
-    }
-
-    if (
-        hasOwnProperty(entity, 'client_secret') &&
-        (
-            typeof entity.client_secret !== 'string' ||
-            !validator.isLength(entity.client_secret, { min: 3, max: 128 })
-        )
-    ) {
-        throw new ValidatorError<OAuth2IdentityProvider>({ key: 'client_secret' });
+    const result = validationSchema.safeParse(entity);
+    if (result.success === false) {
+        throw new BadRequestError(result.error);
     }
 
     return entity;
