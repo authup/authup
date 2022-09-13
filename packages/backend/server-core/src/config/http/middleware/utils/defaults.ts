db8c79f6e13dc523b7e48cc7cb094e1fb86ee32f
tada5hi <peter.placzek1996@gmail.com>
2022-09-13 08:53:57 +0200

    ensure absolute swagger directory path + publish canary version only on push event

--- packages/backend/server-core/src/config/http/middleware/utils/defaults.ts
+++ packages/backend/server-core/src/config/http/middleware/utils/defaults.ts
@@ -9,12 +9,19 @@ import path from 'path';
 import { HTTPMiddlewareOptions } from '../type';
 
 export function extendHTTPMiddlewareOptionsWithDefaults(input: Partial<HTTPMiddlewareOptions>) : HTTPMiddlewareOptions {
+    if (!input.swaggerDirectoryPath) {
+        input.swaggerDirectoryPath = path.join(process.cwd(), 'writable');
+    }
+
+    if (!path.isAbsolute(input.swaggerDirectoryPath)) {
+        input.swaggerDirectoryPath = path.join(process.cwd(), input.swaggerDirectoryPath);
+    }
+
     return {
         bodyParser: input.bodyParser ?? true,
         cookieParser: input.cookieParser ?? true,
         response: input.response ?? true,
         swaggerEnabled: input.swaggerEnabled ?? true,
-        swaggerDirectoryPath: input.swaggerDirectoryPath ||
-            path.join(process.cwd(), 'writable'),
+        swaggerDirectoryPath: input.swaggerDirectoryPath,
     };
 }
