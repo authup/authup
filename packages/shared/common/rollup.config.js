9108704ee081cb9e0384031d7ead8d74cf1a6f1d
tada5hi <peter.placzek1996@gmail.com>
2022-09-21 17:22:14 +0200

    do not minify common package output

--- packages/shared/common/rollup.config.js
+++ packages/shared/common/rollup.config.js
@@ -8,7 +8,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const extensions = [
@@ -43,11 +42,6 @@ export default [
                 presets: [
                     ['@babel/preset-env', { targets: { node: 16 } }],
                 ],
-            }),
-            terser({
-                output: {
-                    comments: false,
-                },
             }),
         ],
         output: [
