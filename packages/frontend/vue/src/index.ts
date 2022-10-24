133096fc6beeebc6ec8cee2f836228a7e12147e9
tada5hi <peter.placzek1996@gmail.com>
2022-10-24 12:41:54 +0200

    style(vue): export composables

--- packages/frontend/vue/src/index.ts
+++ packages/frontend/vue/src/index.ts
@@ -30,3 +30,4 @@ export default install;
 // To allow individual component use, export components
 // each can be registered via Vue.component()
 export * from './components';
+export * from './composables';
