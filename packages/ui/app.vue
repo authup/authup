536cb08fad8e887ec7b334d577dd40bfe685f310
tada5hi <peter.placzek1996@gmail.com>
2023-01-25 16:17:01 +0100

    feat(ui): add initial head meta tags

--- packages/ui/app.vue
+++ packages/ui/app.vue
@@ -1,3 +1,25 @@
+<script>
+import { defineNuxtComponent, useHead } from '#app';
+
+export default defineNuxtComponent({
+    setup() {
+        useHead({
+            charset: 'utf-8',
+            titleTemplate: 'Authup - Authentication & Authorization Framework',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Authup is an authentication & authorization framework. ' +
+                        'It is designed to be easy to use and flexible, with support for multiple authentication strategies.' +
+                        ' With Authup, developers can quickly ' +
+                        'and easily add authentication & authorization to their applications.',
+                },
+            ],
+            link: [],
+        });
+    },
+});
+</script>
 <template>
     <NuxtLayout>
         <NuxtPage />
