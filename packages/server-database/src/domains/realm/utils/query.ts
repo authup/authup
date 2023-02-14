a542df174c9810766a5463099ca313c8c7f8d966
tada5hi <peter.placzek1996@gmail.com>
2023-02-14 15:56:42 +0100

    fix(server-database): readable/writable query resources

--- packages/server-database/src/domains/realm/utils/query.ts
+++ packages/server-database/src/domains/realm/utils/query.ts
@@ -8,26 +8,37 @@
 import { Brackets, SelectQueryBuilder } from 'typeorm';
 import { REALM_MASTER_NAME, Realm, isPropertySet } from '@authup/common';
 
-export function onlyRealmReadableQueryResources<T>(
-    query: SelectQueryBuilder<T>,
-    input: { id: string, name?: string } | string,
-    resourceField: string | string[] = 'realm_id',
-) : void {
-    let realm : Partial<Realm>;
+function prepareRealm(
+    input: Partial<Realm> | string,
+) : Partial<Realm> | undefined {
+    let realm : Partial<Realm> = {};
     if (typeof input === 'string') {
         realm.id = input;
     } else {
         realm = input;
     }
 
+    if (!realm || !realm.id) {
+        return undefined;
+    }
+
     if (
         isPropertySet(realm, 'name') &&
         realm.name === REALM_MASTER_NAME
     ) {
-        return;
+        return undefined;
     }
 
-    if (typeof realm.id === 'undefined') {
+    return realm;
+}
+
+export function onlyRealmReadableQueryResources<T>(
+    query: SelectQueryBuilder<T>,
+    input: Partial<Realm> | string,
+    resourceField: string | string[] = 'realm_id',
+) : void {
+    const realm = prepareRealm(input);
+    if (!realm) {
         return;
     }
 
@@ -35,13 +46,40 @@ export function onlyRealmReadableQueryResources<T>(
         if (Array.isArray(resourceField)) {
             for (let i = 0; i < resourceField.length; i++) {
                 qb.orWhere(
-                    `(${resourceField[i]} = :realm${i} OR ${resourceField} IS NULL)`,
+                    `(${resourceField[i]} = :realm${i} OR ${resourceField[i]} IS NULL)`,
                     { [`realm${i}`]: realm.id },
                 );
             }
         } else {
             qb.where(
                 `(${resourceField} = :realm OR ${resourceField} IS NULL)`,
+                { realm: realm.id },
+            );
+        }
+    }));
+}
+
+export function onlyRealmWritableQueryResources<T>(
+    query: SelectQueryBuilder<T>,
+    input: Partial<Realm> | string,
+    resourceField: string | string[] = 'realm_id',
+) : void {
+    const realm = prepareRealm(input);
+    if (!realm) {
+        return;
+    }
+
+    query.andWhere(new Brackets((qb) => {
+        if (Array.isArray(resourceField)) {
+            for (let i = 0; i < resourceField.length; i++) {
+                qb.orWhere(
+                    `${resourceField[i]} = :realm${i}`,
+                    { [`realm${i}`]: realm.id },
+                );
+            }
+        } else {
+            qb.where(
+                `${resourceField} = :realm`,
                 { realm: realm.id },
             );
         }
