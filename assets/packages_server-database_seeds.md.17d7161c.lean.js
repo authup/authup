438b76415ba8cecec198d61bc961a4cb223b52f0
tada5hi <tada5hi@users.noreply.github.com>
2023-02-28 08:18:40 +0000

    deploy: e2cd6823c3aa52c6cbf56de58ae47919b8a3a5af

--- assets/packages_server-database_seeds.md.17d7161c.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as n,f as l}from"./app.4b10462f.js";const C=JSON.parse('{"title":"Seeds","description":"","frontmatter":{},"headers":[],"relativePath":"packages/server-database/seeds.md"}'),p={name:"packages/server-database/seeds.md"},o=l("",6),e=[o];function t(c,r,y,F,i,D){return a(),n("div",null,e)}const d=s(p,[["render",t]]);export{C as __pageData,d as default};
