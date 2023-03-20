7d8fd5d5ed42db07fefc656be7a38bbc843b59d2
tada5hi <peter.placzek1996@gmail.com>
2023-03-20 10:04:15 +0100

    fix: revert oauth2 protocol validation changes

--- packages/server-database/src/domains/identity-provider/protocol/oauth2/validation.ts
+++ packages/server-database/src/domains/identity-provider/protocol/oauth2/validation.ts
@@ -8,25 +8,23 @@
 import type { OAuth2IdentityProviderBase } from '@authup/common';
 import zod from 'zod';
 
-export const OAuth2IdentityProviderBaseSchema = zod.object({
+const schema = zod.object({
     scope: zod.string().min(3).max(2000).optional()
         .nullable(),
     client_id: zod.string().min(3).max(128),
     client_secret: zod.string().min(3).max(128).optional()
         .nullable(),
-});
 
-export const OAuth2IdentityProviderSchema = zod.object({
     token_url: zod.string().url(),
     token_revoke_url: zod.string().url().optional().nullable(),
     authorize_url: zod.string().url(),
     user_info_url: zod.string().url().optional().nullable(),
-}).merge(OAuth2IdentityProviderBaseSchema);
+});
 
 export function validateOAuth2IdentityProviderProtocol<
     T extends Partial<OAuth2IdentityProviderBase>,
 >(entity: T) : T {
-    const result = OAuth2IdentityProviderSchema.safeParse(entity);
+    const result = schema.safeParse(entity);
     if (result.success === false) {
         throw result.error;
     }
--- packages/server-database/src/domains/identity-provider/protocol/oidc/validation.ts
+++ packages/server-database/src/domains/identity-provider/protocol/oidc/validation.ts
@@ -6,23 +6,12 @@
  */
 
 import type { OpenIDConnectIdentityProvider } from '@authup/common';
-import zod from 'zod';
-import {
-    OAuth2IdentityProviderBaseSchema,
-    OAuth2IdentityProviderSchema,
-} from '../oauth2';
-
-const schema = OAuth2IdentityProviderBaseSchema.merge(zod.object({
-    discovery_endpoint: zod.string().url(),
-})).or(OAuth2IdentityProviderSchema);
+import { validateOAuth2IdentityProviderProtocol } from '../oauth2';
 
 export function validateOidcIdentityProviderProtocol(
     entity: Partial<OpenIDConnectIdentityProvider>,
 ) : Partial<OpenIDConnectIdentityProvider> {
-    const result = schema.safeParse(entity);
-    if (result.success === false) {
-        throw result.error;
-    }
+    validateOAuth2IdentityProviderProtocol(entity);
 
     return entity;
 }
