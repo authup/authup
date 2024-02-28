ac5709ffa867db28b4fdea0ebd0aef5d8a55c9f9
tada5hi <peter.placzek1996@gmail.com>
2024-02-28 15:00:39 +0100

    fix: paring of server core options

--- server/services/core/src/config/parse.ts
+++ server/services/core/src/config/parse.ts
@@ -6,7 +6,7 @@
  */
 
 import zod from 'zod';
-import type { ConfigInput } from './types';
+import type { Config, ConfigInput } from './types';
 import type { DatabaseConnectionOptions } from './utils';
 import { isDatabaseConnectionConfiguration, isDatabaseConnectionConfigurationSupported } from './utils';
 
@@ -51,28 +51,48 @@ export function parseConfig(input: unknown = {}): ConfigInput {
             .or(zod.record(zod.any())).optional(),
         middlewareQuery: zod.boolean()
             .or(zod.record(zod.any())).optional(),
+        middlewarePrometheus: zod.boolean()
+            .or(zod.record(zod.any())).optional(),
+        middlewareRateLimit: zod.boolean()
+            .or(zod.record(zod.any())).optional(),
         middlewareSwagger: zod.boolean().optional(),
-        tokenMaxAgeAccessToken: zod.number()
+
+        tokenAccessMaxAge: zod.number()
             .nonnegative().optional(),
-        tokenMaxAgeRefreshToken: zod.number()
+        tokenRefreshMaxAge: zod.number()
             .nonnegative().optional(),
         registration: zod.boolean().optional(),
         emailVerification: zod.boolean().optional(),
         forgotPassword: zod.boolean().optional(),
 
-        adminUsername: zod.string()
+        clientAuthBasic: zod.boolean().optional(),
+
+        userAuthBasic: zod.boolean().optional(),
+        userAdminEnabled: zod.boolean().optional(),
+        userAdminName: zod.string()
             .min(3)
-            .max(128).optional(),
-        adminPassword: zod.string()
+            .max(128)
+            .optional(),
+        userAdminPassword: zod.string()
             .min(3)
             .max(256).optional(),
-        adminPasswordReset: zod.boolean().optional(),
-        robotEnabled: zod.boolean().optional(),
-        robotSecret: zod.string()
+        userAdminPasswordReset: zod.boolean().optional(),
+
+        robotAuthBasic: zod.boolean().optional(),
+        robotAdminEnabled: zod.boolean().optional(),
+        robotAdminName: zod.string()
+            .min(3)
+            .max(128)
+            .optional(),
+        robotAdminSecret: zod.string()
             .min(3)
             .max(256).optional(),
-        robotSecretReset: zod.boolean().optional(),
-    });
+        robotAdminSecretReset: zod.boolean().optional(),
+
+        permissions: zod.string()
+            .or(zod.array(zod.string()))
+            .optional(),
+    } satisfies Record<keyof Config, any>);
 
     return schema.parse(input);
 }
