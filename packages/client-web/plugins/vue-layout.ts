5b44aaf7bb3133d8ace60cb84a4affd4ff1c85bf
tada5hi <peter.placzek1996@gmail.com>
2024-04-21 19:20:48 +0200

    feat: implement interaction between timeago and ilingo library

--- packages/client-web/plugins/vue-layout.ts
+++ packages/client-web/plugins/vue-layout.ts
@@ -5,7 +5,9 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-import installAuthup from '@authup/client-web-kit';
+import de from 'date-fns/locale/de';
+import { watch } from 'vue';
+import installAuthup, { injectTranslatorLocale } from '@authup/client-web-kit';
 import type { APIClient } from '@authup/core-kit';
 import type { StoreManagerOptions } from '@vuecs/core';
 import bootstrap from '@vuecs/preset-bootstrap-v5';
@@ -15,7 +17,7 @@ import installCountdown from '@vuecs/countdown';
 import installFormControl from '@vuecs/form-controls';
 import installNavigation from '@vuecs/navigation';
 import installPagination from '@vuecs/pagination';
-import installTimeago from '@vuecs/timeago';
+import installTimeago, { injectLocale as injectTimeagoLocale } from '@vuecs/timeago';
 import { applyStoreManagerOptions, installStoreManager } from '@vuecs/form-controls/core';
 
 import type { Pinia } from 'pinia';
@@ -52,7 +54,11 @@ export default defineNuxtPlugin((ctx) => {
 
     ctx.vueApp.use(installCountdown);
     ctx.vueApp.use(installFormControl);
-    ctx.vueApp.use(installTimeago);
+    ctx.vueApp.use(installTimeago, {
+        locales: {
+            de,
+        },
+    });
 
     const store = useAuthStore(ctx.$pinia as Pinia);
     const { loggedIn } = storeToRefs(store);
@@ -71,5 +77,12 @@ export default defineNuxtPlugin((ctx) => {
         apiClient: ctx.$api as APIClient,
         store,
         components: false,
+    });
+
+    const locale = injectTranslatorLocale();
+    const timeagoLocale = injectTimeagoLocale();
+    timeagoLocale.value = locale.value;
+    watch(locale, (val) => {
+        timeagoLocale.value = val;
     });
 });
