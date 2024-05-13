15465b3a766cf66a9a5614aac181a307f1794025
tada5hi <peter.placzek1996@gmail.com>
2024-05-13 15:19:19 +0200

    fix: import utils form kit package

--- packages/authup/src/utils/process-output.ts
+++ packages/authup/src/utils/process-output.ts
@@ -5,7 +5,7 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
-import { hasOwnProperty, isObject } from 'smob';
+import { hasOwnProperty, isObject } from '@authup/kit';
 import { removeLineBreaks } from './line-breaks';
 
 export function parseProcessOutputData(input: unknown) : string[] {
