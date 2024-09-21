8e695889e710fb3dcfb9ed1ea407a29e237e497c
tada5hi <peter.placzek1996@gmail.com>
2024-09-21 18:49:51 +0200

    feat: reogranized cli package

---
+++ packages/authup/src/packages/types.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import type { ChildProcess } from 'node:child_process';
+
+export interface Package {
+    execute(command: string, options: PackageExecuteOptions) : Promise<ChildProcess>;
+}
+
+export type PackageExecuteOptions = {
+    configFile?: string,
+    configDirectory?: string,
+};
