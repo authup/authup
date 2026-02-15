6eece5a3e45dfb259cd1a3a8789103df69098497
tada5hi <peter.placzek1996@gmail.com>
2026-02-15 10:42:05 +0100

    fix: import path

--- packages/server-core/src/app/modules/provisioning/index.ts
+++ packages/server-core/src/app/modules/provisioning/index.ts
@@ -5,8 +5,8 @@
  * view the LICENSE file that was distributed with this source code.
  */
 
+export * from './entities/index.ts';
 export * from './sources/index.ts';
 export * from './synchronizer/index.ts';
 export * from './module.ts';
 export * from './types.ts';
-export * from './entities';
