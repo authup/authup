0422e2a7950d50f3a50bf7b1634855be26231fb8
tada5hi <peter.placzek1996@gmail.com>
2026-03-29 17:56:21 +0200

    test: fix test factory

--- apps/server-core/test/app/factory.ts
+++ apps/server-core/test/app/factory.ts
@@ -55,7 +55,7 @@ export function createTestApplication() : TestHTTPApplication {
         .withHTTP()
         .buildModules();
 
-    return new TestHTTPApplication(modules);
+    return new TestHTTPApplication({ modules });
 }
 
 export function createTestDatabaseApplication() : TestApplication {
@@ -65,5 +65,5 @@ export function createTestDatabaseApplication() : TestApplication {
         .withDatabase(createTestDatabaseModuleForSuite())
         .buildModules();
 
-    return new TestApplication(modules);
+    return new TestApplication({ modules });
 }
